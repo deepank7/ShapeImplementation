a021912ab0483f531da77acd42cece7486167f00
deepank7 <36792699+deepank7@users.noreply.github.com>
2021-02-08 17:00:27 +0530

    Update ShapeHelper.java

--- ShapeHelper.java
+++ ShapeHelper.java
@@ -20,14 +20,5 @@ public class ShapeHelper {
 	      
 	      return null;
 	   }
-	
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		System.out.print("Enter a string: ");
-		String str = sc.nextLine();
-		Shape s =  ShapeHelper.getShape(str);
-		s.getArea();	
-		s.getPerimeter();
-	}
 
 }
